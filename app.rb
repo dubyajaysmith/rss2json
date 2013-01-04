f3147562e391513e5539f8fa3ed249a2b892dfb2
Jeff Mueller <jnmueller@me.com>
2013-01-04 11:37:14 -0800

    Added parsing for RSS -> JSON.

--- app.rb
+++ app.rb
@@ -1,14 +1,34 @@
 require 'sinatra'
+require 'uri'
+require 'net/http'
+require 'json'
+require 'crack'
 
 get '/' do
 
   @feed = params[:feed] if params
-  @count = params[:count] if params
 
   if @feed
-    erb :output
+    return "Invalid Feed URL" if !valid_url?(@feed)
+
+    content_type :json
+    convert_feed(@feed)
   else
     erb :index
   end
 
+end
+
+def convert_feed(feed)
+    xml = Net::HTTP.get_response(URI.parse(@feed)).body
+    Crack::XML.parse(xml).to_json
+end
+
+def valid_url?(feed)
+  url = URI.parse(feed)
+  %w( http https ).include?(url.scheme)
+rescue URI::BadURIError
+  false
+rescue URI::InvalidURIError
+  false
 end
